cb5e5b8e022f8bf2b5c6d73854d82cc5a77a459b
Elliot <elliot_vincent@orange.fr>
2020-04-09 18:44:12 +0200

    Modif by Antoine

---
+++ code/class_partitioning.py
@@ -0,0 +1,76 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Apr  8 16:35:35 2020
+
+@author: antoi
+"""
+import numpy as np
+
+class Partition:
+    
+    def __init__(self, image):
+        self.image = image
+        
+        # Gives a different class to each pixel by default
+        pixels = np.zeros(image.shape)
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                pixels[i,j] = i*image.shape[1] + j
+        self.pixels = pixels
+        
+        # Creates a class for each pixel by default
+        classes = {}
+        for i in range(image.shape[0]):
+            for j in range(image.shape[1]):
+                classes[i*image.shape[1] + j] = [ (i,j) ]
+        self.classes = classes
+        
+    def get_key(self, pixel):
+        return self.pixels[pixel]
+    
+    def merge_classes(self, pixel1, pixel2):
+        # Takes the coordinates of two pixels as tuples, and merges two classes by adding the elements of the smaller one to the other
+        key1, key2 = self.get_key(pixel1), self.get_key(pixel2)
+        if(key1!=key2):
+            class1, class2 = key1, key2
+
+            if len(self.classes[key1]) < len(self.classes[key2]):
+                class1, class2 = key2, key1
+            
+            # Updating the lists in the dictionary
+            self.classes[class1] += self.classes[class2]
+            del self.classes[class2]
+            
+            # Updating the array
+            self.pixels[self.pixels==class2] = class1
+        
+    def get_class(self, pixel):
+        # Takes pixel coordinates as a tuple and returns the list of all pixels in the same class
+        key = self.pixels[pixel]
+        return self.classes[key]
+    
+    def get_all_classes(self):
+        return list(self.classes.values())
+    
+    
+if __name__ == '__main__':
+    nrow, ncol = 10, 10
+    image = np.zeros((nrow, ncol))
+    partition = Partition(image)
+    print("Pixels: ", partition.pixels)
+    print("Classes: ", partition.classes)
+    print(partition.get_all_classes())
+    
+    for i in range(nrow):
+        for j in range(1, ncol):
+            partition.merge_classes((i,0), (i,j))
+    print("Pixels: ", partition.pixels)
+    print("Classes: ", partition.classes)
+    print(partition.get_all_classes())
+    
+    zones = list(partition.get_all_classes())
+    argmax = 0
+    for i in range(0, len(zones)):
+        if len(zones[i]) > len(zones[argmax]):
+            argmax = i
+    print(zones[argmax])
